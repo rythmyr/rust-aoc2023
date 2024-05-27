be38f440b12e94e5583bdcbcfe4a2d3dceef3898
Rythmyr <rythmyr@gmail.com>
2024-05-27 16:33:09 -0500

    day3 - part 2 done!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -86,6 +86,13 @@ fn is_part_number(all_lines: &[Vec<char>], line: usize, start: usize, end: usize
     false
 }
 
+#[derive(Debug)]
+struct AdjacentNum {
+    line: usize,
+    col_start: usize,
+    col_end: usize,
+}
+
 fn part_two() {
     if let Ok(lines) = read_lines("input.txt") {
         let mut sum: usize = 0;
@@ -95,10 +102,99 @@ fn part_two() {
             .collect();
 
         for (l, line) in all_lines.iter().enumerate() {
-            for (c, char) in line.iter().enumerate() {}
+            for (c, char) in line.iter().enumerate() {
+                if *char == '*' {
+                    let adjacent_nums = get_adjacent_nums(&all_lines, l, c);
+                    if adjacent_nums.len() != 2 {
+                        continue;
+                    }
+                    let mut mult: usize = 1;
+                    for adj in &adjacent_nums {
+                        // we know we have 2 numbers, now we just need to know the numbers
+                        let val = (adj.col_start..=adj.col_end)
+                            .map(|x: usize| all_lines[adj.line][x])
+                            .collect::<String>()
+                            .parse::<usize>()
+                            .unwrap();
+                        print!("{}, ", val);
+                        mult *= val;
+                    }
+                    sum += mult;
+                }
+            }
         }
         println!("{}", sum);
     }
+}
+
+fn get_adjacent_nums(
+    all_lines: &[Vec<char>],
+    line_number: usize,
+    col_number: usize,
+) -> Vec<AdjacentNum> {
+    let mut to_return: Vec<AdjacentNum> = vec![];
+    let line_offsets: Vec<usize> = if line_number == 0 {
+        [1, 2].to_vec()
+    } else if line_number == all_lines.len() - 1 {
+        [0, 1].to_vec()
+    } else {
+        [0, 1, 2].to_vec()
+    };
+    let col_offsets: Vec<usize> = if col_number == 0 {
+        [1, 2].to_vec()
+    } else if col_number == all_lines[0].len() - 1 {
+        [0, 1].to_vec()
+    } else {
+        [0, 1, 2].to_vec()
+    };
+
+    for line_offset in line_offsets.iter() {
+        for col_offset in col_offsets.iter() {
+            let l = line_number + line_offset - 1;
+            let mut c = col_number + col_offset - 1;
+
+            let mut char = all_lines[l][c];
+            if !char.is_ascii_digit() {
+                continue;
+            }
+            while c > 0 {
+                c -= 1;
+                char = all_lines[l][c];
+                if !char.is_ascii_digit() {
+                    c += 1;
+                    char = all_lines[l][c];
+                    break;
+                }
+            }
+            let col_start = c;
+            while char.is_ascii_digit() && c < all_lines[0].len() - 1 {
+                c += 1;
+                char = all_lines[l][c];
+                if !char.is_ascii_digit() {
+                    c -= 1;
+                    break;
+                }
+            }
+            let col_end = c;
+            to_return.push(AdjacentNum {
+                col_start,
+                col_end,
+                line: l,
+            });
+        }
+    }
+
+    to_return.sort_by(|num1: &AdjacentNum, num2: &AdjacentNum| {
+        let val1 = num1.line * all_lines[0].len() + num1.col_start;
+        let val2 = num2.line * all_lines[0].len() + num2.col_start;
+        val1.cmp(&val2)
+    });
+
+    to_return.dedup_by(|num1: &mut AdjacentNum, num2: &mut AdjacentNum| {
+        num1.line == num2.line && num1.col_start == num2.col_start && num1.col_end == num2.col_end
+    });
+
+    to_return
 }
 
 // The output is wrapped in a Result to allow matching on errors
