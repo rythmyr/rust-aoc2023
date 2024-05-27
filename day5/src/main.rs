a38cd13d75437a17cd565b6e53a52363aeb91835
Rythmyr <rythmyr@gmail.com>
2024-05-27 16:54:00 -0500

    and remember to copy new template to day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,24 +1,7 @@
-use std::{
-    fs::File,
-    io::{self, BufRead},
-    path::Path,
-};
-
 fn main() {
-    if let Ok(lines) = read_lines("input.txt") {
-        lines.map_while(Result::ok).for_each(|line| {
-            println!("{}", line);
-        });
-    }
-}
+    let lines = include_str!("../input.txt").lines();
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    lines.for_each(|line| {
+        println!("{}", line);
+    });
 }
