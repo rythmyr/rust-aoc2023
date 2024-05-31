3e0dac634fc59d0bfc67b94989f69adacb8807fb
Rythmyr <rythmyr@gmail.com>
2024-05-31 09:01:16 -0500

    day5 - part 1 done!

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,74 @@
-fn main() {
-    let lines = include_str!("../input.txt").lines();
+struct Map {
+    in_start: usize,
+    out_start: usize,
+    len: usize,
+}
 
-    lines.for_each(|line| {
-        println!("{}", line);
-    });
+impl Map {
+    fn map(&self, seed: &usize) -> usize {
+        assert!(self.contains(seed));
+        *seed - self.in_start + self.out_start
+    }
+
+    fn contains(&self, seed: &usize) -> bool {
+        (self.in_start..(self.in_start+self.len)).contains(seed)
+    }
+}
+
+fn main() {
+    let lines: Vec<&str> = include_str!("../input.txt").lines().collect();
+    // Vector of references to slices of references to str
+    let groups: Vec<&[&str]> = lines.split(|s| s.is_empty()).collect::<Vec<_>>();
+    let mut groups_iter = groups.iter();
+
+    let str_vec = groups_iter.next().unwrap();
+    let seeds_line = str_vec.first().unwrap();
+
+    // TODO parse seed numbers
+    let (_, seeds_str) = seeds_line.split_once(": ").unwrap();
+    let seeds: Vec<usize> = seeds_str
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let map_groups: Vec<Vec<Map>> = groups_iter
+        .map(|group| {
+            let mut group_iter = group.iter();
+            let _ = group_iter.next().unwrap(); // get the map name line out
+
+            group_iter
+                .map(|map_str| {
+                    let map_nums: Vec<usize> = map_str
+                        .split_whitespace()
+                        .map(|s| s.parse::<usize>().unwrap())
+                        .collect();
+                    Map {
+                        in_start: map_nums[1],
+                        out_start: map_nums[0],
+                        len: map_nums[2],
+                    }
+                })
+                .collect()
+        })
+        .collect();
+
+    // apply the maps
+    let new_seeds = map_groups
+        .iter()
+        .fold(seeds, |seeds, map_group: &Vec<Map>| {
+            let new_seeds = seeds.into_iter().map(|seed| -> usize {
+                let map_opt = map_group.iter().find(|map| map.contains(&seed));
+                if let Some(map) = map_opt {
+                    map.map(&seed)
+                } else {
+                    seed
+                }
+            }).collect();
+            println!("{:?}", new_seeds);
+            new_seeds
+        });
+
+    println!("{:?}", new_seeds);
+
+    println!("{}", new_seeds.iter().min().unwrap());
 }
